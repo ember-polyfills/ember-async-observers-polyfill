bf2d696dfea694a49d7787e90552eba68b6c8460
Robert Jackson <rjackson@linkedin.com>
2019-12-20 17:52:08 -0500

    yarn lint:js --fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,15 +8,8 @@ module.exports = {
       legacyDecorators: true
     }
   },
-  plugins: [
-    'ember',
-    'prettier',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['ember', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:ember/recommended', 'plugin:prettier/recommended'],
   env: {
     browser: true
   },
@@ -36,12 +29,7 @@ module.exports = {
         'config/**/*.js',
         'tests/dummy/config/**/*.js'
       ],
-      excludedFiles: [
-        'addon/**',
-        'addon-test-support/**',
-        'app/**',
-        'tests/dummy/app/**'
-      ],
+      excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
       parserOptions: {
         sourceType: 'script'
       },
